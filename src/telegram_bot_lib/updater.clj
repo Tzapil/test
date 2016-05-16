9e32b81dd3d43e1714e3fa96fd4c7cc47692fdd8
Tzapil <thhwitcher@gmail.com>
2016-05-17 00:05:55 +0300

    update

--- src/telegram_bot_lib/updater.clj
+++ src/telegram_bot_lib/updater.clj
@@ -3,26 +3,47 @@
               [telegram-bot-lib.bot :as bot]
               [telegram-bot-lib.helpers :as helpers]))
 
-(defn _start_polling [token c offset]
-    (async/go-loop []
-            (try
-                (let [updates (bot/get_updates token @offset)
-                      json (helpers/body_json updates)
-                      updates (count json)]
-                    (if (> updates 0)
-                        (async/>!! c json)))
-                (catch Exception e
-                    (str "Caught exception: " (.getMessage e))))
+(defn _start_polling [token c]
+    (async/go-loop [offset 0]
             (async/<! (async/timeout 1000))   ;; 1 sec pause
-            (recur)))
+            (recur (try
+                        (let [updates (bot/get_updates token offset)
+                              json (helpers/body_json updates)
+                              updates (count json)]
+                            (if (= updates 0)
+                                offset
+                                (do
+                                    (async/>! c json)
+                                    (inc (reduce #(max %1 (get %2 :update_id)) offset json)))))
+                        (catch Exception e
+                            (println (str "Caught exception: " (.getMessage e)))
+                            offset))))
+    c)
 
-(defn start_polling [token handler]
-    (let [c (async/chan)
-          offset (atom nil)]
-        (_start_polling token c offset)
-        (async/go-loop []
-            (let [data (async/<! c)]
-                (println "Got a value in this loop:" data)
-                (handler data)
-                (reset! offset (inc (reduce #(max %1 (get %2 :update_id)) 0 data)))
-                (recur)))))
+(defn _handle [json [handler & other]]
+    (println "TEST")
+    (if ((:pr handler) json)
+            ((:f handler) json)
+            (if (> (count other) 0)
+                (_handle json other))))
+
+(defn start_handlers [handlers c]
+    (async/go-loop []
+        (let [data (async/<! c)]
+            (println "Got a value in this loop:" data)
+            (doseq [json data]
+                (_handle json handlers))
+            (recur)))
+    c)
+
+(defn start_polling [token]
+    (let [c (async/chan)]
+        (_start_polling token c)))
+
+(defn idle 
+    ([] 
+        (idle 1000))
+    ([timeout] 
+        (loop []
+            (async/<!! (async/timeout timeout))
+            (recur))))

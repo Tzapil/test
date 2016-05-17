0f8a72cb8de048526e6e919c6869cc58a0824bc7
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-05-17 11:19:37 +0300

    fix

--- src/telegram_bot_lib/handlers.clj
+++ src/telegram_bot_lib/handlers.clj
@@ -1,13 +1,10 @@
 (ns telegram-bot-lib.handlers)
 
 (defn check_command [n t]
-    (println "REG")
-    (println n)
-    (println t)
     (let [r (re-pattern (str "/" n "(([@\\s].*)|$)"))]
         (not (nil? (re-matches r t)))))
 
-(defn add_command [n f] {
-        :pr #((println (str "SHASAH: " %)) (check_command n (get-in % [:message :text])))
+(defn create_command [n f] {
+        :pr #(check_command n (get-in % [:message :text]))
         :f f
     })

91dbb6eb707118352da20c5acbf4b3020587c28d
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-05-17 18:55:09 +0300

    update

--- src/telegram_bot_lib/handlers.clj
+++ src/telegram_bot_lib/handlers.clj
@@ -1,10 +1,7 @@
-(ns telegram-bot-lib.handlers)
-
-(defn check_command [n t]
-    (let [r (re-pattern (str "/" n "(([@\\s].*)|$)"))]
-        (not (nil? (re-matches r t)))))
+(ns telegram-bot-lib.handlers
+    :require [clojure.core.filters :as filters]))
 
 (defn create_command [n f] {
-        :pr #(check_command n (get-in % [:message :text]))
+        :pr #(filters/command n (get-in % [:message :text]))
         :f f
     })

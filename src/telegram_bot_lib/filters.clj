00bc23d365d237315bb7ebbace2d4bd42d27f0d5
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-05-18 19:08:01 +0300

    filters

--- src/telegram_bot_lib/filters.clj
+++ src/telegram_bot_lib/filters.clj
@@ -1,10 +1,54 @@
 (ns telegram-bot-lib.filters)
 
+(def not-nil? (complement nil?))
+
 (defn acommand [n t]
     (let [r (re-pattern (str "/" n "(([@\\s].*)|$)"))]
         (and (not (nil? t)) (not (nil? (re-matches r t))))))
 
-(defn command [m]
-    (and (not (nil? m)) 
-         (let [t (get-in m [:message :text])]
-            (and (not (nil? t)) (starts-with? t "/")))))
+(defn command [u]
+    (let [t (get-in u [:message :text])]
+        (and (not-nil? t) (starts-with? t "/"))))
+
+(defn text [u]
+    (let [t (get-in u [:message :text])]
+        (and (not-nil? t) (not (starts-with? t "/")))))
+
+(defn audio [u]
+    (let [a (get-in u [:message :audio])]
+        (not-nil? a)))
+
+(defn document [u]
+    (let [d (get-in u [:message :document])]
+        (not-nil? d))))
+
+(defn sticker [u]
+    (let [s (get-in u [:message :sticker])]
+        (not-nil? s))))
+
+(defn video [u]
+    (let [v (get-in u [:message :video])]
+        (not-nil? v))))
+
+(defn voice [u]
+    (let [v (get-in u [:message :voice])]
+        (not-nil? v))))
+
+(defn contact [u]
+    (let [c (get-in u [:message :contact])]
+        (not-nil? c))))
+
+(defn location [u]
+    (let [l (get-in u [:message :location])]
+        (not-nil? l))))
+
+(defn venue [u]
+    (let [v (get-in u [:message :venue])]
+        (not-nil? v))))
+
+(defn status_update [su]
+    (let [message (get su :message)
+          status [:new_chat_member :left_chat_member :new_chat_title :new_chat_photo :delete_chat_photo
+                  :group_chat_created :supergroup_chat_created :channel_chat_created :migrate_to_chat_id 
+                  :migrate_from_chat_id :pinned_message]]
+        (not-nil? v))))

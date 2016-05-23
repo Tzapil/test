e0db2d2b96073ebd1fd6932296f070c9a12a9e86
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-05-23 18:35:14 +0300

    remove json dependency

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.2.0"]
                  [cheshire "5.5.0"]
-                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.async "0.2.374"]]
   :main ^:skip-aot telegram-bot-lib.core
   :target-path "target/%s"

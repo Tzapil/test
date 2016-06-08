9ee3d56862a780d5f33a682c35827f0d56419eca
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-06-08 19:00:09 +0300

    update

--- src/telegram_bot_lib/inline.clj
+++ src/telegram_bot_lib/inline.clj
@@ -18,22 +18,114 @@
             }
         }))
 
+(defn create_result_photo
+    ([photo_url thumb_url]
+        (create_result_photo photo_url thumb_url nil nil))
+    ([photo_url thumb_url title]
+        (create_result_photo photo_url thumb_url title nil))
+    ([photo_url thumb_url title description]
+        {
+            :type "photo"
+            :id (helpers/uuid)
+            :title title
+            :photo_url photo_url
+            :thumb_url thumb_url
+            :description description
+        }))
+
+(defn create_result_gif
+    ([gif_url thumb_url]
+        (create_result_gif gif_url thumb_url nil nil))
+    ([gif_url thumb_url title]
+        (create_result_gif gif_url thumb_url title nil))
+    ([gif_url thumb_url title caption]
+        {
+            :type "gif"
+            :id (helpers/uuid)
+            :title title
+            :gif_url gif_url
+            :thumb_url thumb_url
+            :caption caption
+        }))
+
+(defn create_result_mpeg4_gif
+    ([mpeg4_url thumb_url]
+        (create_result_mpeg4_gif mpeg4_url thumb_url title nil nil))
+    ([mpeg4_url thumb_url title]
+        (create_result_mpeg4_gif mpeg4_url thumb_url title nil))
+    ([mpeg4_url thumb_url title caption]
+        {
+            :type "mpeg4_gif"
+            :id (helpers/uuid)
+            :title title
+            :mpeg4_url mpeg4_url
+            :thumb_url thumb_url
+            :caption caption
+        }))
+
+(defn create_result_video
+    ([video_url mime_type thumb_url title]
+        (create_result_video video_url mime_type thumb_url title nil nil))
+    ([video_url mime_type thumb_url title caption]
+        (create_result_video video_url mime_type thumb_url title caption nil))
+    ([video_url mime_type thumb_url title caption description]
+        {
+            :type "video"
+            :id (helpers/uuid)
+            :title title
+            :video_url video_url
+            :mime_type mime_type
+            :thumb_url thumb_url
+            :caption caption
+            :description description
+        }))
+
 (defn create_result_audio
-    ([title audio_url]
-        (create_result_audio title audio_url nil nil))
-    ([title audio_url message_text]
-        (create_result_audio title audio_url message_text nil))
-    ([title audio_url message_text parse_mode]
+    ([audio_url title]
+        (create_result_audio audio_url title nil))
+    ([audio_url title performer]
         {
             :type "audio"
             :id (helpers/uuid)
             :title title
             :audio_url audio_url
-            :input_message_content {
-                :message_text message_text
-                :parse_mode parse_mode       
-            }
+            :performer performer
         }))
+
+(defn create_result_voice
+    [voice_url title]
+        {
+            :type "voice"
+            :id (helpers/uuid)
+            :title title
+            :voice_url voice_url
+        })
+
+(defn create_result_document
+    ([document_url title mime_type]
+        (create_result_document document_url title mime_type nil nil))
+    ([document_url title mime_type caption]
+        (create_result_document title document_url title mime_type caption nil))
+    ([document_url title mime_type caption description]
+        {
+            :type "document"
+            :id (helpers/uuid)
+            :title title
+            :document_url document_url
+            :mime_type mime_type
+            :caption caption
+            :description description
+        }))
+
+(defn create_result_location
+    [latitude longitude title]
+        {
+            :type "location"
+            :id (helpers/uuid)
+            :title title
+            :latitude latitude
+            :longitude longitude
+        })
 
 (defn create_result_contact
     ([phone_number first_name]
@@ -46,59 +138,6 @@
             :id (helpers/uuid)
             :phone_number phone_number
             :first_name first_name
-            :input_message_content {
-                :message_text message_text
-                :parse_mode parse_mode       
-            }
-        }))
-
-(defn create_result_gif
-    ([title gif_url]
-        (create_result_gif title gif_url nil nil))
-    ([title gif_url message_text]
-        (create_result_gif title gif_url message_text nil))
-    ([title gif_url message_text parse_mode]
-        {
-            :type "gif"
-            :id (helpers/uuid)
-            :title title
-            :gif_url gif_url
-            :input_message_content {
-                :message_text message_text
-                :parse_mode parse_mode       
-            }
-        }))
-
-(defn create_result_document
-    ([title document_url mime_type]
-        (create_result_document title document_url mime_type nil nil))
-    ([title document_url mime_type message_text]
-        (create_result_document title document_url mime_type message_text nil))
-    ([title document_url mime_type message_text parse_mode]
-        {
-            :type "document"
-            :id (helpers/uuid)
-            :title title
-            :document_url document_url
-            :mime_type mime_type
-            :input_message_content {
-                :message_text message_text
-                :parse_mode parse_mode       
-            }
-        }))
-
-(defn create_result_location
-    ([title latitude longitude]
-        (create_result_location title latitude longitude nil nil))
-    ([title latitude longitude message_text]
-        (create_result_location title latitude longitude message_text nil))
-    ([title latitude longitude message_text parse_mode]
-        {
-            :type "location"
-            :id (helpers/uuid)
-            :title title
-            :latitude latitude
-            :longitude longitude
             :input_message_content {
                 :message_text message_text
                 :parse_mode parse_mode       

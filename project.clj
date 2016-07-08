35b53800d19084ed733d17a7e2980eb5b5a27f60
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2016-07-08 13:42:56 +0300

    move files to library

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject telegram-bot-lib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Simple library to create bots with telegram API"
   :url "http://example.com/FIXME"
   :license {:name "GPLv3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
@@ -9,7 +9,8 @@
                  [org.clojure/core.async "0.2.374"]
                  [ring/ring-core "1.5.0-RC1"]
                  [ring/ring-jetty-adapter "1.5.0-RC1"]
-                 [org.clojure/data.zip "0.1.2"]]
+                 [org.clojure/data.zip "0.1.2"]
+                 [tblibrary "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot telegram-bot-lib.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
